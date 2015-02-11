b761186bf8821a5691f5cfb430c52d5b6984e7d9
Barbara Wahl <wahl@hanover.edu>
2015-02-11 16:24:04 -0500

    Trial version of gamma rng.

--- distributions/gamma.js
+++ distributions/gamma.js
@@ -33,10 +33,12 @@ define(function(require) {
             if (u2 <= (2 - x) / (2 + x) || u2 <= Math.exp(-x)) {
                return x;
             }
+         } else {
             x = -Math.log(t * (b - v) / alpha);
             y = x / t;
             if (u2 * (alpha + y * (1 - alpha)) <= 1 ||
-                u2 <= Math.pow(y, alpha - 1)) {
+                u2 <= Math.pow(y, alpha - 1))
+            {
                return x;
             }
          }
@@ -45,16 +47,37 @@ define(function(require) {
 
    // alpha > 1:  Cheng/Feast Algorithm
    function gammaCheng(alpha, beta) {
-      var u1, u2, v;
+      var u1, u2, v, c2;
       /* eslint-disable no-constant-condition */
       while (true) {
       /* eslint-enable */
          u1 = this.random();
          u2 = this.random();
-         v = (alpha - 1 / (6 * alpha)) * u1 / ((alpha - 1) * u2);
+         c2 = (alpha - 1 / (6 * alpha)) / (alpha - 1);
+         v = c2 * u1 / u2;
          if (2 * (u2 - 1) / (alpha - 1) + v + 1 / v <= 2 ||
-             2 * Math.log(u2) / (alpha - 1) - Math.log(v) + v <= 1) {
+             2 * Math.log(u2) / (alpha - 1) - Math.log(v) + v < 1) {
             return (alpha - 1) * v;
+         }
+      }
+   }
+
+   // alpha > 1:  trying again with more local variables
+   function gammaCheng2(alpha, beta) {
+      var c1, c2, c3, c4, u1, u2, w;
+      c1 = alpha - 1;
+      c2 = (alpha - 1 / (6 * alpha)) / c1;
+      c3 = 2 / c1;
+      c4 = c3 + 2;
+      while (true) {
+         u1 = this.random();
+         u2 = this.random();
+         w = c2 * u2 / u1;
+         if (c3 * u1 + w + 1 / w <= c4) {
+            return c1 * w;
+         }
+         if (c3 * Math.log(u1) - Math.log(w) + w < 1) {
+            return c1 * w;
          }
       }
    }

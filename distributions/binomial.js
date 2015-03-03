c1e9b8da9a982cf51f093167aa983ba47700189c
Haris Skiadas <cskiadas@gmail.com>
2015-03-02 20:17:18 -0500

    Add inverseCDF version of Bernoulli. Close #22

--- distributions/binomial.js
+++ distributions/binomial.js
@@ -3,6 +3,33 @@ define(function(require) {
 
 // binomial.js
 
+   var lbinomProb = require('panthrMath').lbinomProb,
+       inverseCDF = require('./inverseCDF');
+
+   // Using inverseCDF
+   function bernoulliInvCDF(n, p) {
+      var mode;
+
+      mode = { val: Math.floor(n * p) };
+      mode.prob = Math.exp(lbinomProb(n, p, mode.val));
+
+      return inverseCDF.call(this,
+         function getMode() { return mode; },
+         function updateLeft() {
+            if (this.val === 0) { return false; }
+            this.prob *= this.val / (n - this.val + 1) * ((1 - p) / p);
+            this.val -= 1;
+            return true;
+         },
+         function updateRight() {
+            if (this.val === n) { return false; }
+            this.prob *= (n - this.val) / (this.val + 1) * (p / (1 - p));
+            this.val += 1;
+            return true;
+         }
+      );
+   }
+
    // Sum of repeated Bernoulli trials
    function bernoulliDirect(n, p) {
       return function() {
@@ -19,7 +46,7 @@ define(function(require) {
       if (n < 30) {
          return bernoulliDirect(n, p).bind(this);
       }
-      return bernoulliDirect(n, p).bind(this);
+      return bernoulliInvCDF.call(this, n, p);
    };
 });
 
